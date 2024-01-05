15eb542a5c188e8dba5472ae1ee26e1668e9686a
pouriya ghasemi <pouriya.ghasemi1@gmail.com>
2024-01-05 16:52:24 +0100

    add student repository

--- src/1.Core/BD.CourseApp.Core.Domain/Entities/Student.cs
+++ src/1.Core/BD.CourseApp.Core.Domain/Entities/Student.cs
@@ -6,6 +6,6 @@ namespace BD.CourseApp.Core.Domain.Entities
     {
         public Guid StudentId { get; set; }
         public string? Name { get; set; }
-        public FrozenSet<Course>? Students { get; set; }
+        public FrozenSet<Course>? Courses { get; set; }
     }
 }
---
+++ src/2.Infrastructures/Data/BD.CourseApp.Infrastructures.Data.SqlServer/Repositories/StudentRepository.cs
@@ -0,0 +1,45 @@
+﻿using BD.CourseApp.Core.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BD.CourseApp.Infrastructures.Data.SqlServer.Repositories
+{
+    public class StudentRepository
+    {
+        private string _connectionString;
+
+        public StudentRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public List<Student> GetAllStudents()
+        {
+            var students = new List<Student>();
+
+            using SqlConnection connection = new SqlConnection(_connectionString);
+
+            connection.Open();
+            SqlCommand command = new SqlCommand("SELECT * FROM Students", connection);
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    var student = new Student
+                    {
+                        StudentId = new Guid(reader["StudentId"].ToString()),
+                        Name = reader["Name"].ToString()
+                    };
+                    students.Add(student);
+                }
+            }
+            connection.Close();
+
+            return students;
+        }
+    }
+}
